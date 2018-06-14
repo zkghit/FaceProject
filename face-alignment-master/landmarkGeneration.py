7a0b477d9ceb343fdc03c27d205e37c86ba38eb8
zkghit <kg.zhang@aggiemail.usu.edu>
2018-06-13 19:18:33 -0700

    Add files via upload

---
+++ face-alignment-master/landmarkGeneration.py
@@ -0,0 +1,24 @@
+import argparse
+import face_alignment
+from skimage import io
+# from skimage.transform import resize
+import os
+import numpy as np
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--cuda', type = bool, default=False)
+parser.add_argument('--load_dir', type = str, default='../Facephoto/')
+opt = parser.parse_args()
+
+if __name__ == "__main__":
+	fa = face_alignment.FaceAlignment(face_alignment.LandmarksType._2D, enable_cuda=False, flip_input=False)
+	for file in os.listdir(opt.load_dir):
+		if file.endswith(".jpg"):
+			input = io.imread(opt.load_dir+file)
+			# imResize = resize(input,(200,200))
+			preds = fa.get_landmarks(input)
+			name = file.replace('.jpg','.npy')
+			np.save(name, preds[0])
+			nameJ = file.replace('.npy','.jpg')
+			io.imsave(nameJ, input)
+
